ffb21166b6c75b0b6ce538d1dd17da1ed3b43346
Linh Vu Van <linhvuvan2022@gmail.com>
2023-11-03 11:01:04 +0700

    faster startup script

--- infra/src/buildMetadata.ts
+++ infra/src/buildMetadata.ts
@@ -21,11 +21,18 @@ type Metadata = {
 export const buildStartupScript = () => {
   return `
     #!/bin/bash
-    START=$(date +%s)
-    docker pull google/cloud-sdk:alpine
-    docker run -t  google/cloud-sdk:alpine gcloud secrets versions access latest --secret=wms >> /tmp/.env
-    END=$(date +%s)
-    echo "Startup script completed in $((END-START)) seconds"
+    PROJECT_ID=linhvuvan-397815
+    SECRET_NAME=wms
+    SECRET_VERSION=latest
+    
+    ACCESS_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header "Metadata-Flavor: Google" | jq -r '.access_token')
+
+    curl "https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/$SECRET_VERSION:access" \
+      --request "GET" \
+      --header "Authorization: Bearer $ACCESS_TOKEN" \
+      --header "content-type: application/json" \
+      | jq -r ".payload.data" | base64 --decode \
+      >> /tmp/.env
   `;
 };
 
