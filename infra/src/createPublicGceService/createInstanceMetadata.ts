1b599d668d9abeeb2dab2657b3ce0a55c12628fc
Linh Vu Van <linhvuvan2022@gmail.com>
2023-11-10 09:54:25 +0700

    format

--- infra/src/createPublicGceService/createInstanceMetadata.ts
+++ infra/src/createPublicGceService/createInstanceMetadata.ts
@@ -7,8 +7,8 @@ type CreateInstanceMetadataParams = {
 
 export const createInstanceMetadata = (
   params: CreateInstanceMetadataParams,
-) => {
-  const container = yaml.stringify({
+) => ({
+  'gce-container-declaration': yaml.stringify({
     spec: {
       containers: [
         {
@@ -41,22 +41,18 @@ export const createInstanceMetadata = (
       ],
       restartPolicy: 'Always',
     },
-  });
+  }),
+  'google-logging-enabled': 'true',
+  'google-logging-use-fluentbit': 'true',
+  'startup-script': `
+    #!/bin/bash
+    ACCESS_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header "Metadata-Flavor: Google" | jq -r '.access_token')
 
-  return {
-    'gce-container-declaration': container,
-    'google-logging-enabled': 'true',
-    'google-logging-use-fluentbit': 'true',
-    'startup-script': `
-      #!/bin/bash
-      ACCESS_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header "Metadata-Flavor: Google" | jq -r '.access_token')
-
-      curl "https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/latest:access" \
-        --request "GET" \
-        --header "Authorization: Bearer $ACCESS_TOKEN" \
-        --header "content-type: application/json" \
-        | jq -r ".payload.data" | base64 --decode \
-        >> /tmp/.env
-    `,
-  };
-};
+    curl "https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/latest:access" \
+      --request "GET" \
+      --header "Authorization: Bearer $ACCESS_TOKEN" \
+      --header "content-type: application/json" \
+      | jq -r ".payload.data" | base64 --decode \
+      >> /tmp/.env
+  `,
+});
