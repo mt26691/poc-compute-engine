9ae6376a10a00d70c69e7179b976439fbabeccef
Linh Vu Van <linhvuvan2022@gmail.com>
2023-11-10 09:54:37 +0700

    format

--- infra/src/createPublicGceService/createInstanceMetadata.ts
+++ infra/src/createPublicGceService/createInstanceMetadata.ts
@@ -7,52 +7,54 @@ type CreateInstanceMetadataParams = {
 
 export const createInstanceMetadata = (
   params: CreateInstanceMetadataParams,
-) => ({
-  'gce-container-declaration': yaml.stringify({
-    spec: {
-      containers: [
-        {
-          name: params.image.name,
-          image: params.image.url,
-          stdin: false,
-          tty: false,
-          env: [
-            {
-              name: 'DB_USER',
-              value: 'root',
-            },
-          ],
-          volumeMounts: [
-            {
-              name: 'env',
-              mountPath: '/app/.env',
-              readOnly: true,
-            },
-          ],
-        },
-      ],
-      volumes: [
-        {
-          name: 'env',
-          hostPath: {
-            path: '/tmp/.env',
+) => {
+  return {
+    'gce-container-declaration': yaml.stringify({
+      spec: {
+        containers: [
+          {
+            name: params.image.name,
+            image: params.image.url,
+            stdin: false,
+            tty: false,
+            env: [
+              {
+                name: 'DB_USER',
+                value: 'root',
+              },
+            ],
+            volumeMounts: [
+              {
+                name: 'env',
+                mountPath: '/app/.env',
+                readOnly: true,
+              },
+            ],
           },
-        },
-      ],
-      restartPolicy: 'Always',
-    },
-  }),
-  'google-logging-enabled': 'true',
-  'google-logging-use-fluentbit': 'true',
-  'startup-script': `
-    #!/bin/bash
-    ACCESS_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header "Metadata-Flavor: Google" | jq -r '.access_token')
+        ],
+        volumes: [
+          {
+            name: 'env',
+            hostPath: {
+              path: '/tmp/.env',
+            },
+          },
+        ],
+        restartPolicy: 'Always',
+      },
+    }),
+    'google-logging-enabled': 'true',
+    'google-logging-use-fluentbit': 'true',
+    'startup-script': `
+      #!/bin/bash
+      ACCESS_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header "Metadata-Flavor: Google" | jq -r '.access_token')
 
-    curl "https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/latest:access" \
-      --request "GET" \
-      --header "Authorization: Bearer $ACCESS_TOKEN" \
-      --header "content-type: application/json" \
-      | jq -r ".payload.data" | base64 --decode \
-      >> /tmp/.env
-  `,
-});
+      curl "https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/latest:access" \
+        --request "GET" \
+        --header "Authorization: Bearer $ACCESS_TOKEN" \
+        --header "content-type: application/json" \
+        | jq -r ".payload.data" | base64 --decode \
+        >> /tmp/.env
+    `,
+  };
+};
