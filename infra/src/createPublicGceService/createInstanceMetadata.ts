29615a84bb1eabbd1a5ae9c21d817fb8de6f471d
Linh Vu Van <linhvuvan2022@gmail.com>
2023-11-10 09:53:44 +0700

    format

--- infra/src/createPublicGceService/createInstanceMetadata.ts
+++ infra/src/createPublicGceService/createInstanceMetadata.ts
@@ -1,35 +1,13 @@
 import * as yaml from 'yaml';
-import * as pulumi from '@pulumi/pulumi';
 import { Image } from './';
 
-export const APP_NAME = 'compute-engine-app';
-export const config = new pulumi.Config('poc-compute-engine');
-
-const IMAGE_REPOSITORY = `gcr.io/linhvuvan-image-holder/${APP_NAME}:33`;
-
-const createStartupScript = () => {
-  return `
-    #!/bin/bash
-    PROJECT_ID=linhvuvan-397815
-    SECRET_NAME=wms
-    SECRET_VERSION=latest
-    
-    ACCESS_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header "Metadata-Flavor: Google" | jq -r '.access_token')
-
-    curl "https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/$SECRET_VERSION:access" \
-      --request "GET" \
-      --header "Authorization: Bearer $ACCESS_TOKEN" \
-      --header "content-type: application/json" \
-      | jq -r ".payload.data" | base64 --decode \
-      >> /tmp/.env
-  `;
-};
-
 type CreateInstanceMetadataParams = {
-  image: Image
+  image: Image;
 };
 
-export const createInstanceMetadata = (params: CreateInstanceMetadataParams) => {
+export const createInstanceMetadata = (
+  params: CreateInstanceMetadataParams,
+) => {
   const container = yaml.stringify({
     spec: {
       containers: [
@@ -39,10 +17,6 @@ export const createInstanceMetadata = (params: CreateInstanceMetadataParams) => 
           stdin: false,
           tty: false,
           env: [
-            {
-              name: 'DB_NAME',
-              value: config.get('dbName'),
-            },
             {
               name: 'DB_USER',
               value: 'root',
@@ -73,6 +47,16 @@ export const createInstanceMetadata = (params: CreateInstanceMetadataParams) => 
     'gce-container-declaration': container,
     'google-logging-enabled': 'true',
     'google-logging-use-fluentbit': 'true',
-    'startup-script': createStartupScript(),
+    'startup-script': `
+      #!/bin/bash
+      ACCESS_TOKEN=$(curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token --header "Metadata-Flavor: Google" | jq -r '.access_token')
+
+      curl "https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/$SECRET_NAME/versions/latest:access" \
+        --request "GET" \
+        --header "Authorization: Bearer $ACCESS_TOKEN" \
+        --header "content-type: application/json" \
+        | jq -r ".payload.data" | base64 --decode \
+        >> /tmp/.env
+    `,
   };
 };
