8e86d2329c4501150344b646d840a73273aaaed9
Linh Vu Van <linhvuvan2022@gmail.com>
2023-11-15 21:01:24 +0700

    comment

--- infra/src/index.ts
+++ infra/src/index.ts
@@ -1,34 +1,35 @@
+import * as pulumi from '@pulumi/pulumi';
 import { createPublicGceService } from './createPublicGceService';
 
-createPublicGceService({
-  resourcePrefix: 'poc-compute-engine',
-  image: {
-    url: 'gcr.io/tat-den/poc-compute-engine:33',
-  },
-  containerPort: 3000,
-  startTimeSec: 10,
-  numberOfInstances: 1,
-  healthCheck: {
-    httpHealthCheck: {
-      port: 3000,
-      requestPath: '/healthz',
-    },
-  },
-  secret: {
-    project: 'chi-dau',
-    name: 'poc-compute-engine',
-  },
-  instance: {
-    baseName: 'poc-compute-engine',
-    roles: [
-      {
-        role: 'roles/secretmanager.secretAccessor',
-        project: 'chi-dau',
-      },
-      {
-        role: 'roles/storage.objectViewer',
-        project: 'tat-den',
-      },
-    ],
-  },
-});
+// createPublicGceService({
+//   resourcePrefix: 'poc-compute-engine',
+//   image: {
+//     url: 'gcr.io/tat-den/poc-compute-engine:33',
+//   },
+//   containerPort: 3000,
+//   startTimeSec: 10,
+//   numberOfInstances: 1,
+//   healthCheck: {
+//     httpHealthCheck: {
+//       port: 3000,
+//       requestPath: '/healthz',
+//     },
+//   },
+//   secret: {
+//     project: 'chi-dau',
+//     name: 'poc-compute-engine',
+//   },
+//   instance: {
+//     baseName: 'poc-compute-engine',
+//     roles: [
+//       {
+//         role: 'roles/secretmanager.secretAccessor',
+//         project: 'chi-dau',
+//       },
+//       {
+//         role: 'roles/storage.objectViewer',
+//         project: 'tat-den',
+//       },
+//     ],
+//   },
+// });
