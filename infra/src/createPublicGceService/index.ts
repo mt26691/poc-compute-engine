7141fd9c49e9ecfdee409f7c521c71aabad9543b
Linh Vu Van <linhvuvan2022@gmail.com>
2023-11-21 12:09:29 +0700

    random string

--- infra/src/createPublicGceService/index.ts
+++ infra/src/createPublicGceService/index.ts
@@ -1,6 +1,6 @@
 import { createPublicGceService } from './createPublicGceService';
 
-const imageUrl = 'gcr.io/tat-den/poc-compute-engine:19';
+const imageUrl = 'gcr.io/tat-den/poc-compute-engine:20';
 
 createPublicGceService({
   resourcePrefix: 'poc-compute-engine',
