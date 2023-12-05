0ad33594646e0a15ea305a2d60fd98170e269ec5
Linh Vu Van <linhvuvan2022@gmail.com>
2023-12-05 12:39:55 +0700

    destroy

--- infra/src/index.ts
+++ infra/src/index.ts
@@ -1,21 +1,21 @@
-import './createPublicGceService';
-import { createTopic } from './utils/createTopic';
-import { subscribeTopic } from './utils/subscribeTopic';
+// import './createPublicGceService';
+// import { createTopic } from './utils/createTopic';
+// import { subscribeTopic } from './utils/subscribeTopic';
 
-const topic = createTopic({
-  resourcePrefix: 'linhvuvan',
-});
+// const topic = createTopic({
+//   resourcePrefix: 'linhvuvan',
+// });
 
-subscribeTopic({
-  topic: topic.name,
-  pushEndpoint: 'https://linhvuvan.com/webhook-1',
-  subscriber: 'webhook-1',
-  publisher: 'linhvuvan',
-});
+// subscribeTopic({
+//   topic: topic.name,
+//   pushEndpoint: 'https://linhvuvan.com/webhook-1',
+//   subscriber: 'webhook-1',
+//   publisher: 'linhvuvan',
+// });
 
-subscribeTopic({
-  topic: topic.name,
-  pushEndpoint: 'https://linhvuvan.com/webhook-2',
-  subscriber: 'webhook-2',
-  publisher: 'linhvuvan',
-});
+// subscribeTopic({
+//   topic: topic.name,
+//   pushEndpoint: 'https://linhvuvan.com/webhook-2',
+//   subscriber: 'webhook-2',
+//   publisher: 'linhvuvan',
+// });
