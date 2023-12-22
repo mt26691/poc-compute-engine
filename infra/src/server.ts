333541c40e9e39bc74b71ca0feebd5df09acece8
Linh Vu Van <linhvuvan2022@gmail.com>
2023-12-22 10:01:00 +0700

    remove client

---
+++ infra/src/server.ts
@@ -0,0 +1,60 @@
+import { createPublicGceService } from './createPublicGceService';
+
+const imageUrl = 'gcr.io/tat-den/poc-compute-engine:28';
+const PORT = 3000;
+
+createPublicGceService({
+  resourcePrefix: 'poc-compute-engine',
+  image: {
+    url: imageUrl,
+    context: '../server',
+  },
+  machineType: 't2d-standard-1',
+  containerPort: PORT,
+  initialStartupDelaySec: 30,
+  numberOfInstances: 1,
+  healthCheck: {
+    httpHealthCheck: {
+      port: PORT,
+      requestPath: '/healthz',
+    },
+    checkIntervalSec: 10,
+    healthyThreshold: 1,
+    unhealthyThreshold: 1,
+  },
+  env: [
+    {
+      name: 'REVISION',
+      value: imageUrl,
+    },
+    {
+      name: 'PORT',
+      value: String(PORT),
+    },
+  ],
+  secret: {
+    project: 'chi-dau',
+    name: 'poc-compute-engine',
+  },
+  instance: {
+    baseName: 'poc-compute-engine',
+    roles: [
+      {
+        role: 'roles/secretmanager.secretAccessor',
+        project: 'chi-dau',
+      },
+      {
+        role: 'roles/logging.logWriter',
+        project: 'chi-dau',
+      },
+      {
+        role: 'roles/storage.objectViewer',
+        project: 'tat-den',
+      },
+    ],
+  },
+  domain: 'linhvuvan.com',
+  managedZone: 'linhvuvan-com',
+  project: 'chi-dau',
+  region: 'us-central1',
+});
