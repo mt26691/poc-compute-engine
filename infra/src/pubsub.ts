239c976777649742f554ef84255ce664d9766192
Linh Vu Van <linhvuvan2022@gmail.com>
2023-12-22 10:29:16 +0700

    add sub

--- infra/src/pubsub.ts
+++ infra/src/pubsub.ts
@@ -2,6 +2,16 @@ import * as pulumi from '@pulumi/pulumi';
 import * as gcp from '@pulumi/gcp';
 import { serviceName } from './server';
 
-new gcp.pubsub.Topic(`${serviceName}-topic`, {
+const topic = new gcp.pubsub.Topic(`${serviceName}-topic`, {
   name: serviceName,
 });
+
+const subscription = new gcp.pubsub.Subscription(
+  `${serviceName}-subscription`,
+  {
+    topic: topic.name,
+    pushConfig: {
+      pushEndpoint: `https://linhvuvan.com/webhook`,
+    },
+  },
+);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { pubsub } from './pubsub';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const TOPIC_NAME = process.env.TOPIC_NAME || 'poc-compute-engine';
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -10,6 +12,22 @@ app.get('/healthz', (req, res) => {
   return res.status(200).json({
     message: 'ok',
     revision: process.env.REVISION,
+  });
+});
+
+app.post('/event', async (req, res) => {
+  await pubsub.topic(TOPIC_NAME).publishMessage(req.body);
+
+  return res.status(200).json({
+    message: 'ok',
+  });
+});
+
+app.post('/webhook', async (req, res) => {
+  console.log(req.body);
+
+  return res.status(200).json({
+    message: 'ok',
   });
 });
 
