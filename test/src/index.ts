ef1b356b0745620c8e79d682d6f9e2ea821eb845
Linh Vu Van <linhvuvan2022@gmail.com>
2023-12-25 20:40:51 +0700

    change test

--- test/src/index.ts
+++ test/src/index.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 (async () => {
   for (let i = 0; i < 100; i++) {
-    console.log('attempt', i);
-
-    await axios.post('https://linhvuvan.com/event', {
-      hi: 'there',
+    const res = await axios.post('https://linhvuvan.com/event', {
+      hi: 'linhvuvan',
       attempt: i,
     });
+
+    console.log('attempt', i, res.data);
   }
 })();
