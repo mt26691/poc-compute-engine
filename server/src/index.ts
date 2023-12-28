e1b58b4401e356862ac9a50a3878fb6f766fbf17
Linh Vu Van <linhvuvan2022@gmail.com>
2023-12-28 10:28:15 +0700

    deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,8 +7,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const TOPIC_NAME = process.env.TOPIC_NAME || 'unset';
 const SUBSCRIPTION_NAME = process.env.SUBSCRIPTION_NAME || 'unset';
-let subscription_one: Subscription;
-let subscription_two: Subscription;
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -51,39 +49,13 @@ const createMessageHandler =
     message.ack();
   };
 
-// listen for new messages
-subscription_one = pubsub
+const subscription = pubsub
   .subscription(SUBSCRIPTION_NAME, {
-    flowControl: {
-      maxMessages: 1,
-    },
+    flowControl: { maxMessages: 1 },
   })
-  .on('message', createMessageHandler('subscription_one'));
-
-subscription_one.on('error', (error) => {
-  console.error('subscription_one error', error);
-});
-
-subscription_one.on('close', () => {
-  console.log('subscription_one subscription closed');
-});
-
-// listen for new messages
-subscription_two = pubsub
-  .subscription(SUBSCRIPTION_NAME, {
-    flowControl: {
-      maxMessages: 1,
-    },
-  })
-  .on('message', createMessageHandler('subscription_two'));
-
-subscription_two.on('error', (error) => {
-  console.error('subscription_two error', error);
-});
-
-subscription_two.on('close', () => {
-  console.log('subscription_two subscription closed');
-});
+  .on('message', createMessageHandler(Math.random().toString()))
+  .on('error', console.error)
+  .on('close', () => console.log('subscription closed'));
 
 app.get('/healthz', (req, res) => {
   return res.status(200).json({
@@ -107,15 +79,8 @@ app.post('/event', async (req, res) => {
 });
 
 app.post('/pubsub/open', (req, res) => {
-  console.log('/pubsub/open', req.body);
-
-  if (req.body.subscriptions.includes('subscription_one')) {
-    subscription_one.open();
-  }
-
-  if (req.body.subscriptions.includes('subscription_two')) {
-    subscription_two.open();
-  }
+  console.log('/pubsub/open');
+  subscription.open();
 
   return res.status(200).json({
     message: 'ok',
@@ -123,15 +88,8 @@ app.post('/pubsub/open', (req, res) => {
 });
 
 app.post('/pubsub/close', async (req, res) => {
-  console.log('/pubsub/close', req.body);
-
-  if (req.body.subscriptions.includes('subscription_one')) {
-    await subscription_one.close();
-  }
-
-  if (req.body.subscriptions.includes('subscription_two')) {
-    await subscription_two.close();
-  }
+  console.log('/pubsub/close');
+  await subscription.close();
 
   return res.status(200).json({
     message: 'ok',
