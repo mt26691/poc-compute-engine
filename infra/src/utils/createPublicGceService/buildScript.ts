2d7d7094e8e72277301c360a5feedab52b1e4c4d
Linh Vu Van <linhvuvan2022@gmail.com>
2024-01-03 11:47:56 +0700

    more echo

--- infra/src/utils/createPublicGceService/buildScript.ts
+++ infra/src/utils/createPublicGceService/buildScript.ts
@@ -24,11 +24,6 @@ export const buildStartupScript = (params: BuildStartupScriptParams) => {
       --header "content-type: application/json" \
       | jq -r ".payload.data" | base64 --decode \
       >> ${params.secretVolume.hostPath}
-
-    mkdir -p /etc/systemd/system/docker.service.d
-    printf "[Service]\nExecStop=/bin/sh -c 'docker ps -q | xargs docker stop --signal TERM --time 60'\n" >> /etc/systemd/system/docker.service.d/override.conf
-    systemctl daemon-reload
-    systemctl restart docker
   `;
 };
 
@@ -51,6 +46,10 @@ export const buildShutdownScript = () => {
 
     echo "======================================= stopping container another way"
     docker ps -q | xargs docker stop
+    echo "======================================= stopped container"
+
     docker ps -a
+    
+    echo "======================================= end of shutdown script"
   `;
 };
