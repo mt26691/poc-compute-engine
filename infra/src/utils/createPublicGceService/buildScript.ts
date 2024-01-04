23fac1b2de71067a42267a0911dfc831f6ac9d81
Linh Vu Van <linhvuvan2022@gmail.com>
2024-01-04 09:14:48 +0700

    remove bin

--- infra/src/utils/createPublicGceService/buildScript.ts
+++ infra/src/utils/createPublicGceService/buildScript.ts
@@ -26,7 +26,7 @@ export const buildStartupScript = (params: BuildStartupScriptParams) => {
       >> ${params.secretVolume.hostPath}
 
     mkdir -p /etc/systemd/system/docker.service.d
-    printf "[Service]\nExecStop=/bin/sh -c 'docker ps -q | xargs docker stop --time 60'\n" > /etc/systemd/system/docker.service.d/override.conf
+    printf "[Service]\nExecStop=docker ps -q | xargs docker stop --time 60" > /etc/systemd/system/docker.service.d/override.conf
     systemctl daemon-reload
     systemctl restart docker
   `;
